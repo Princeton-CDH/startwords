df421f8c4696f3a12e1726d60e3aef048780c5b6
Nick Budak <nbudak@princeton.edu>
2020-06-23 15:49:34 -0400

    faster nav hiding; don't hide on desktop (#66)

--- themes/startwords/assets/js/nav.js
+++ themes/startwords/assets/js/nav.js
@@ -2,13 +2,14 @@
 // https://github.com/Princeton-CDH/ppa-django/blob/master/srcmedia/js/pitbar.js
 document.addEventListener('DOMContentLoaded', function() {
     var nav = document.querySelector('nav[aria-label=main]')
+    var mobile = window.matchMedia('(max-width: 768px)')
     var scroll = 0
 
-    if (nav) document.addEventListener('scroll', checkScroll)
+    if (nav && mobile.matches) document.addEventListener('scroll', checkScroll)
 
     function checkScroll() {
         var scrolled = Math.abs(document.body.getBoundingClientRect().top)
-        if (scrolled - scroll > 25 && scrolled > scroll && scrolled > 90) {
+        if (scrolled > scroll) {
             if (!nav.classList.contains('hidden')) {
                 nav.classList.add('hidden')
             }
