f23ec06bad2aca8bd83662520d993804fd9e1b41
Nick Budak <nbudak@princeton.edu>
2020-06-25 14:57:36 -0400

    make nav behavior respect window resizing (#66)

--- themes/startwords/assets/js/nav.js
+++ themes/startwords/assets/js/nav.js
@@ -2,11 +2,9 @@
 // https://github.com/Princeton-CDH/ppa-django/blob/master/srcmedia/js/pitbar.js
 document.addEventListener('DOMContentLoaded', function() {
     var nav = document.querySelector('nav[aria-label=main]')
-    var mobile = window.matchMedia('(max-width: 768px)')
     var scroll = 0
 
-    if (nav && mobile.matches) document.addEventListener('scroll', checkScroll)
-
+    // hide the nav element when scrolling down; show when scrolling up quickly
     function checkScroll() {
         var scrolled = Math.abs(document.body.getBoundingClientRect().top)
         if (scrolled > scroll) {
@@ -20,5 +18,16 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
         scroll = scrolled
-    }    
+    }
+
+    // only use the hide/show behavior if the screen matches mobile media query
+    function setHandler() {
+        var mobile = window.matchMedia('(max-width: 768px)')
+        if (mobile.matches) document.addEventListener('scroll', checkScroll)
+        else document.removeEventListener('scroll', checkScroll) 
+    }
+    
+    // apply the behavior on page load and re-apply if window is resized
+    setHandler()
+    window.addEventListener('resize', setHandler)
 })
