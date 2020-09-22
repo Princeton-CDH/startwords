48536249ced330ef215fdcdffe43c142911723c9
Nick Budak <nbudak@princeton.edu>
2020-09-22 15:34:34 -0400

    Update lighthouse ci config (#122)
    - turn off contrast validation for some pages with incorrect errors
    - test against the 404 page
    - run 3 times against each URL for consistency

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,26 +1,42 @@
 module.exports = {
-    ci: {
-      collect: {
-        numberOfRuns: 1,
-        staticDistDir: "./public",
-        url: [
-            "/",
-            "/about/",
-            "/issues/",
-            "/issues/1/",
-            "/issues/1/data-beyond-vision/"
-        ]
-      },
-      assert: {
-        assertions: {
-            // warn if overall performance score is lower than 85
-            "categories:performance": ["warn", { minScore: 0.85 }],
-            // error if overall accessibility score is lower than 100
-            "categories:accessibility": ["error", { minScore: 1 }],
-        }
-      },
-      upload: {
-        target: "temporary-public-storage",
-      },
+  ci: {
+    collect: {
+      staticDistDir: "./public",
+      url: [
+        "/",
+        "/about/",
+        "/issues/",
+        "/issues/1/",
+        "/issues/1/data-beyond-vision/",
+        "/404.html"
+    ]
     },
-  };
+    assert: {
+      assertMatrix: [
+        // homepage/issue detail incorrectly error on contrast of background
+        // text with role=presentation
+        {
+          "matchingUrlPattern": "http://[^/]+/$",
+          assertions: { "color-contrast": "off", }
+        },
+        {
+          "matchingUrlPattern": "http://[^/]+/issues/\d+/$",
+          assertions: { "color-contrast": "off", }
+        },
+        {
+          // global settings for all pages
+          "matchingUrlPattern": ".*",            
+          assertions: {
+              // warn if overall performance score is lower than 85
+              "categories:performance": ["warn", { minScore: 0.85 }],
+              // error if overall accessibility score is lower than 100
+              "categories:accessibility": ["error", { minScore: 1 }],
+          }
+        },
+      ]
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
