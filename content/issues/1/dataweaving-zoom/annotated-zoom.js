00760ca65a336fe0fca3c67cfec10e30b2fa74f6
rlskoeser <rebecca.s.koeser@princeton.edu>
2020-10-17 10:48:25 -0400

    Revise styles and logic for annotated weaving

--- content/issues/1/dataweaving-zoom/annotated-zoom.js
+++ content/issues/1/dataweaving-zoom/annotated-zoom.js
@@ -1,137 +1,72 @@
+/// todos
+// - disable scroll on contextual note
+// - position contextual note better vertically
+// - disable back link in notes? (doesn't work now / hard to make work)
+// - grab images for texts that have them, style images
+
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
 
-    var viewer = OpenSeadragon({
-        id:"dataweaving-zoom",
-        prefixUrl:"https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/",
-        preserveViewport: true,
-        visibilityRatio: 1,
-        // disallow zoom in/out
-        minZoomLevel: 4,
-        maxZoomLevel: 4,
-        defaultZoomLevel: 4,
-        // disable vertical panning
-        panVertical: false,
-        showNavigationControl: false,
-        // show nav overview in top left
-        showNavigator:  true,
-        navigatorPosition:   "TOP_LEFT",
-        tileSources:"https:\/\/iiif.princeton.edu\/loris\/iiif\/2\/figgy_prod%2F58%2F51%2Fd4%2F5851d48b225b42699a13181c778a6095%2Fintermediate_file.jp2\/info.json",
-        overlays: [
-        {
-            // page1 label
-            id: 'p1',
-            px: 2600,
-            py: 650,
-            width: 50,
-            height: 50,
-            className: 'pnum'
-        },
+    // load annotation overlays and then use to initialize viewer
+    fetch('annotations.json')
+      .then(response => response.json())
+      .then(data => initAnnotatedZoom(data));
 
-        {
-            id: 'p2',
-            px: 3793,
-            py: 650,
-            width: 50,
-            height: 50,
-            className: 'pnum'
-        },
-        {
-            id: 'p3',
-            px: 4840,
-            py: 650,
-            width: 50,
-            height: 50,
-            className: 'pnum'
-        },
-        {
-            id: 'p4',
-            px: 5862,
-            py: 650,
-            width: 50,
-            height: 50,
-            className: 'pnum'
-        },
-        {
-            id: 'p5',
-            px: 6210,
-            py: 650,
-            width: 50,
-            height: 50,
-            className: 'pnum'
-        },
-        {
-            id: 'p6',
-            px: 6570,
-            py: 650,
-            width: 50,
-            height: 50,
-            className: 'pnum'
-        },
+    function initAnnotatedZoom(data) {
 
-        {
-            // epigram p15
-            id: 'fnref:1', // ref15a',
-            px: 2560,
-            py: 848,
-            width: 352,
-            height: 1776,
-            className: 'highlight'
-        },
-        {
-            // footnote 1 p16
-            id: 'fnref:2',
-            px: 4359,
-            py: 810,
-            width: 141,
-            height: 1794,
-            className: 'highlight'
-        },
-        {
-            // footnote 2 p16
-            id: 'fnref:3',
-            px: 4500,
-            py: 810,
-            width: 141,
-            height: 1794,
-            className: 'highlight'
-        },
-        {
-            // footnote 3 p16
-            id: 'ref16c',
-            px: 4642,
-            py: 810,
-            width: 141,
-            height: 1794,
-            className: 'highlight'
-        },
-    ],
-    });
+        // create elements with page numbers to be positioned as overlays
+        var pageDiv = document.getElementById('page_numbers');
+        data.overlays.filter(el => el.className == 'page_n')
+            .forEach(el => {
+                const pageSpan = document.createElement('span');
+                pageSpan.innerText = el.content;
+                pageSpan.setAttribute('id', el.id);
+                pageDiv.appendChild(pageSpan);
+            });
 
-    viewer.addHandler("open", function () {
-        // start with left part of the weaving showing
-        viewer.viewport.panTo(new OpenSeadragon.Point(0.17, 0.09), true)
-
-        // equivalent event for touch?
-        const referenceOverlays = document.querySelectorAll(".highlight");
+        var viewer = OpenSeadragon({
+            id:"dataweaving-zoom",
+            prefixUrl:"https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/",
+            preserveViewport: true,
+            visibilityRatio: 1,
+            // disallow zoom in/out
+            minZoomLevel: 4,
+            maxZoomLevel: 4,
+            defaultZoomLevel: 4,
+            // disable vertical panning
+            panVertical: false,
+            showNavigationControl: false,
+            // show nav overview in top left
+            showNavigator:  true,
+            navigatorPosition:   "TOP_LEFT",
+            tileSources:"https:\/\/iiif.princeton.edu\/loris\/iiif\/2\/figgy_prod%2F58%2F51%2Fd4%2F5851d48b225b42699a13181c778a6095%2Fintermediate_file.jp2\/info.json",
+            overlays: data.overlays,
+        });
 
         function showReference() {
-            console.log(this.getAttribute('id'));
+            // trigger contextual note via hash
             location.hash = '#' + this.getAttribute('id').replace('ref', '');
         }
 
-        for (const refOverlay of referenceOverlays) {
-            refOverlay.addEventListener('touchstart', showReference)
-           refOverlay.addEventListener('click', showReference)
-        }
+        viewer.addHandler("open", function () {
+            // start with left part of the weaving showing
+            viewer.viewport.panTo(new OpenSeadragon.Point(0.17, 0.09), true)
 
-    });
+            // on touch or click for reference zone, trigger contextual note
+            const referenceOverlays = document.querySelectorAll(".highlight");
+            for (const refOverlay of referenceOverlays) {
+                refOverlay.addEventListener('touchstart', showReference)
+               refOverlay.addEventListener('click', showReference)
+            }
 
-    // close any open contextual notes when panning through the image
-    viewer.addHandler("pan", function () {
-        location.hash = '#-';
-    });
+        });
+
+        // close any open contextual notes when panning through the image
+        viewer.addHandler("pan", function () {
+            location.hash = '#-';
+        });
+
+    }
 
 });
