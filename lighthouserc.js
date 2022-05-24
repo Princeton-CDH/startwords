0c8e32cbcf5f18f57e76f7d5f93aa3bb4c9a32f8
rlskoeser <rebecca.s.koeser@princeton.edu>
2022-05-24 16:45:35 -0400

    Add author list page to lighthouse checks

--- lighthouserc.js
+++ lighthouserc.js
@@ -19,18 +19,19 @@ module.exports = {
         "/issues/2/datas-destinations/",
         "/issues/2/creating-and-recreating-virtual-community/",
         "/issues/2/serendipity-in-the-cairo-geniza/",
+        "/authors/",
         "/404.html"
     ]
     },
     assert: {
       preset: "lighthouse:no-pwa",
       assertions: {
-        /* Can't find an explicit mapping from mailures to IDs. 
+        /* Can't find an explicit mapping from mailures to IDs.
         However, in the Lighthouse interface, the div's ID is the key.
         A list of SOME tests:
-        https://github.com/GoogleChrome/lighthouse-ci/blob/72107f3bf462ab60596f576967ff1a5e0aad622b/packages/utils/src/presets/all.js 
+        https://github.com/GoogleChrome/lighthouse-ci/blob/72107f3bf462ab60596f576967ff1a5e0aad622b/packages/utils/src/presets/all.js
         IDs are also available in GH Actions output. */
-        
+
         /* color contrast check reports incorrect results on issue page and
            on 404 page footer due to the way content is actually rendered; so
            we switch it to a warning */
