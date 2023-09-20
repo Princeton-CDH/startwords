6a7509789b00b3fc67b4a7e67646676324129eb4
rlskoeser <rebecca.s.koeser@princeton.edu>
2023-09-20 16:43:47 -0400

    Provide visual feedback when audio is playing

--- content/issues/4/sonorous-medieval/audio.js
+++ content/issues/4/sonorous-medieval/audio.js
@@ -1,11 +1,21 @@
 /* simple audio play button logic for figure 6 */
 window.onload = (event) => {
-  const audioPlayButtons = document.querySelectorAll("button.play");
+    const audioPlayButtons = document.querySelectorAll("button.play");
 
-  audioPlayButtons.forEach((button) => {
-      button.addEventListener("click", (event) => {
-        const audio = event.target.previousElementSibling;
-        audio.play();
-  });
-});
+    audioPlayButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            const audio = event.target.previousElementSibling;
+            audio.play();
+        });
+    });
+
+    const audio = document.querySelectorAll("audio");
+    audio.forEach((audio) => {
+        audio.addEventListener("play", (event) => {
+            event.target.classList.add("playing");
+        });
+        audio.addEventListener("ended", (event) => {
+            event.target.classList.remove("playing");
+        });
+    });
 };
