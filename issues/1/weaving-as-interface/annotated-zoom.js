35b19e925af984f68eeeeddd55c351bcb7a4ba5a
rlskoeser <rlskoeser@users.noreply.github.com>
2023-10-02 19:39:32 +0000

    deploy: 926d25c0f7d1261b230ae6220bb305036b12f9dc

--- issues/1/weaving-as-interface/annotated-zoom.js
+++ issues/1/weaving-as-interface/annotated-zoom.js
@@ -1,105 +1,23 @@
-/// todos
+"use strict";function _createForOfIteratorHelper(o,allowArrayLike){var it=typeof Symbol!=="undefined"&&o[Symbol.iterator]||o["@@iterator"];if(!it){if(Array.isArray(o)||(it=_unsupportedIterableToArray(o))||allowArrayLike&&o&&typeof o.length==="number"){if(it)o=it;var i=0;var F=function F(){};return{s:F,n:function n(){if(i>=o.length)return{done:true};return{done:false,value:o[i++]}},e:function e(_e){throw _e},f:F}}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}var normalCompletion=true,didErr=false,err;return{s:function s(){it=it.call(o)},n:function n(){var step=it.next();normalCompletion=step.done;return step},e:function e(_e2){didErr=true;err=_e2},f:function f(){try{if(!normalCompletion&&it["return"]!=null)it["return"]()}finally{if(didErr)throw err}}}}function _unsupportedIterableToArray(o,minLen){if(!o)return;if(typeof o==="string")return _arrayLikeToArray(o,minLen);var n=Object.prototype.toString.call(o).slice(8,-1);if(n==="Object"&&o.constructor)n=o.constructor.name;if(n==="Map"||n==="Set")return Array.from(o);if(n==="Arguments"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n))return _arrayLikeToArray(o,minLen)}function _arrayLikeToArray(arr,len){if(len==null||len>arr.length)len=arr.length;for(var i=0,arr2=new Array(len);i<len;i++)arr2[i]=arr[i];return arr2}/// todos
 // - disable scroll when positioning contextual note
 // - position contextual note better vertically
 // - disable back link in notes? (doesn't work now / not sure how to make work)
 // - fix or remove broken images
 // - link to books on derrida's margins site?
-
-
-window.addEventListener('DOMContentLoaded', (event) => {
-
-    // load annotation overlays and then use to initialize viewer
-    fetch('annotations.json')
-      .then(response => response.json())
-      .then(data => initAnnotatedZoom(data));
-
-    function initAnnotatedZoom(data) {
-        // create elements with page numbers to be positioned as overlays
-        var frag = document.createDocumentFragment();
-        data.overlays.filter(el => el.className == 'page_n')
-            .forEach(el => {
-                const pageSpan = document.createElement('span');
-                pageSpan.innerText = el.content;
-                pageSpan.setAttribute('id', el.id);
-                frag.appendChild(pageSpan);
-            });
-        // create containers for note references to use as overlays
-        // then move note references inside them
-        data.overlays.filter(el => el.className.includes('highlight'))
-            .forEach(el => {
-                const refZone = document.createElement('div');
-                refZone.setAttribute('id', el.id);
-                refZone.appendChild(document.getElementById('fn' + el.id));
-                frag.appendChild(refZone);
-            });
-
-        document.getElementById('overlays').appendChild(frag);
-
-        var zoomLevel = 2;
-
-        var viewer = OpenSeadragon({
-            id:"dataweaving-zoom",
-            prefixUrl:"https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/",
-            preserveViewport: true,
-            visibilityRatio: 1,
-            defaultZoomLevel: zoomLevel,
-            // disallow zoom in/out — disable zooming by click or scroll
-            zoomPerClick: 1.0,
-            zoomPerScroll: 1.0,
-            // disable vertical panning
-            panVertical: false,
-            showNavigationControl: false,
-            // show nav overview in top left
-            showNavigator:  true,
-            navigatorPosition:   "TOP_LEFT",
-            tileSources:"https:\/\/iiif.princeton.edu\/loris\/iiif\/2\/figgy_prod%2F58%2F51%2Fd4%2F5851d48b225b42699a13181c778a6095%2Fintermediate_file.jp2\/info.json",
-            overlays: data.overlays,
-            immediateRender: true,
-            viewportMargins: {
-                top: 25,
-                bottom: 25
-            }
-        });
-
-
-
-        function showReference() {
-            // trigger contextual note via hash
-            location.hash = '#' + this.getAttribute('id').replace('ref', 'fn');
-        }
-
-        function adjustZoom() {
-            // show the full height of the weaving image,
-            // with space for page number above and references below
-            // fit the image vertically, then zoom in slightly
-            viewer.viewport.fitVertically(true);
-            viewer.viewport.zoomBy(1.25);
-        }
-
-        viewer.addHandler("open", function () {
-            // adjust initial zoom
-            adjustZoom();
-            // start with left part of the weaving showing
-            viewer.viewport.panTo(new OpenSeadragon.Point(0.17, 0.09), true)
-
-            // on touch or click for reference zone, trigger contextual note
-            const referenceOverlays = document.querySelectorAll(".highlight");
-            for (const refOverlay of referenceOverlays) {
-                refOverlay.addEventListener('touchstart', showReference)
-               refOverlay.addEventListener('click', showReference)
-            }
-        });
-
-        // close any open contextual notes when panning through the image
-        viewer.addHandler("pan", function () {
-            ContextualNotes.closeNote();
-        });
-
-        // adjust zoom fit when the window is resized
-        window.addEventListener('resize', function() {
-            adjustZoom()
-        });
-
-    }
-
-});
+window.addEventListener("DOMContentLoaded",function(event){// load annotation overlays and then use to initialize viewer
+fetch("annotations.json").then(function(response){return response.json()}).then(function(data){return initAnnotatedZoom(data)});function initAnnotatedZoom(data){// create elements with page numbers to be positioned as overlays
+var frag=document.createDocumentFragment();data.overlays.filter(function(el){return el.className=="page_n"}).forEach(function(el){var pageSpan=document.createElement("span");pageSpan.innerText=el.content;pageSpan.setAttribute("id",el.id);frag.appendChild(pageSpan)});// create containers for note references to use as overlays
+// then move note references inside them
+data.overlays.filter(function(el){return el.className.includes("highlight")}).forEach(function(el){var refZone=document.createElement("div");refZone.setAttribute("id",el.id);refZone.appendChild(document.getElementById("fn"+el.id));frag.appendChild(refZone)});document.getElementById("overlays").appendChild(frag);var zoomLevel=2;var viewer=OpenSeadragon({id:"dataweaving-zoom",prefixUrl:"https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/",preserveViewport:true,visibilityRatio:1,defaultZoomLevel:zoomLevel,// disallow zoom in/out — disable zooming by click or scroll
+zoomPerClick:1,zoomPerScroll:1,// disable vertical panning
+panVertical:false,showNavigationControl:false,// show nav overview in top left
+showNavigator:true,navigatorPosition:"TOP_LEFT",tileSources:"https://iiif.princeton.edu/loris/iiif/2/figgy_prod%2F58%2F51%2Fd4%2F5851d48b225b42699a13181c778a6095%2Fintermediate_file.jp2/info.json",overlays:data.overlays,immediateRender:true,viewportMargins:{top:25,bottom:25}});function showReference(){// trigger contextual note via hash
+location.hash="#"+this.getAttribute("id").replace("ref","fn")}function adjustZoom(){// show the full height of the weaving image,
+// with space for page number above and references below
+// fit the image vertically, then zoom in slightly
+viewer.viewport.fitVertically(true);viewer.viewport.zoomBy(1.25)}viewer.addHandler("open",function(){// adjust initial zoom
+adjustZoom();// start with left part of the weaving showing
+viewer.viewport.panTo(new OpenSeadragon.Point(0.17,0.09),true);// on touch or click for reference zone, trigger contextual note
+var referenceOverlays=document.querySelectorAll(".highlight");var _iterator=_createForOfIteratorHelper(referenceOverlays),_step;try{for(_iterator.s();!(_step=_iterator.n()).done;){var refOverlay=_step.value;refOverlay.addEventListener("touchstart",showReference);refOverlay.addEventListener("click",showReference)}}catch(err){_iterator.e(err)}finally{_iterator.f()}});// close any open contextual notes when panning through the image
+viewer.addHandler("pan",function(){ContextualNotes.closeNote()});// adjust zoom fit when the window is resized
+window.addEventListener("resize",function(){adjustZoom()})}});
